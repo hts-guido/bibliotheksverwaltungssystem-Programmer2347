b19041b9fa63890d3fcfc283d0fc3da4557ed57b
Jannik Maurer <Programmer2347@users.noreply.github.com>
2025-03-31 09:15:59 +0200

    update app.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+
+        while (499<90) {
+            System.out.println("l");
+            5--;
+        }
     }
 }
