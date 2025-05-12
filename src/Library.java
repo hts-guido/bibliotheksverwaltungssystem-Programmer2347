3dd5609cfd64ebf738fd40d9c880f5f444d6cb3f
Jannik Maurer <Programmer2347@users.noreply.github.com>
2025-05-12 09:19:01 +0200

    lib setup

--- src/Library.java
+++ src/Library.java
@@ -1,6 +1,21 @@
 public class Library{
 
     private ArrayList<Book> books = new ArrayList<>();
-
     
+    public void addBook(Book book) {
+        books.add(book);
+    }
+    
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+    public Book searchBook(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null; // Buch nicht gefunden
+    }
 }
